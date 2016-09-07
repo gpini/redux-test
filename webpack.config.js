154c38a69460fdeb239decbc11e435ee9df0e1d4
Giovanni Pini <giovanni.pini@phoops.it>
2016-09-07 10:06:54 +0200

    Change dist folder and HtmlPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,23 @@
 var webpack = require('webpack');
 var path = require('path');
 var ExtractTextPlugin = require("extract-text-webpack-plugin");
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 var DEBUG = process.env.NODE_ENV !== 'production' ? true : false;
 
 var APP_DIR = path.resolve(__dirname, 'app');
-var BUILD_PATH = 'bin';
+var BUILD_PATH = 'dist';
 var BUILD_DIR = path.resolve(__dirname, BUILD_PATH);
 var CSS_FILE_NAME = 'styles.css';
 var extractTextPlugin = new ExtractTextPlugin(CSS_FILE_NAME);
+var htmlPlugin = new HtmlWebpackPlugin({
+  template: 'index.html'
+});
 
 var config = {
   entry: APP_DIR + '/index.js',
   output: {
     path: BUILD_DIR,
     filename: 'bundle.js',
-    publicPath: '/' + BUILD_PATH + '/'
   },
   module: {
     loaders: [{
@@ -37,21 +40,26 @@ var config = {
         // inline css for hot loader support
         'style!css?sourceMap!sass?sourceMap' :
         // extractes css for distribution
-        ExtractTextPlugin.extract('style-loader', 'css-loader', 'sass-loader')
+        ExtractTextPlugin.extract('style', 'css!sass')
     }, {
       test: /\.css$/,
       loader: DEBUG ?
         // inline css for hot loader support
         'style!css?sourceMap' :
         // extractes css for distribution
-        ExtractTextPlugin.extract('style!css')
+        ExtractTextPlugin.extract('style', 'css')
     }]
   },
   plugins: DEBUG ? [
-    extractTextPlugin
+    htmlPlugin
   ] : [
     extractTextPlugin,
-    new webpack.optimize.UglifyJsPlugin()
+    htmlPlugin,
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false
+      }
+    })
   ]
 };
 
